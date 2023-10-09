aeccba8766b649a97e409a2a3b91a5d7f43e519d
Iliès BELDJILALI <ilies.beldjilali@cartier.com>
2023-10-09 19:27:03 +0200

    feat: intelliJ conf + strategy example

---
+++ strategy/Main.kt
@@ -0,0 +1,21 @@
+class Printer(private val stringFormatterStrategy: (String) -> String) {
+    fun print(string: String) {
+        println(stringFormatterStrategy(string))
+    }
+}
+
+val reverse  = { s: String -> s.reversed() }
+
+val lowercase : (String) -> String  = String::lowercase
+
+val uppercase = { it: String -> it.uppercase() }
+
+val test = fun(s: String) = s.reversed()
+
+// Usage
+
+
+fun main(args: Array<String>) {
+    val printer = Printer(uppercase)
+    printer.print("Hello Takima !")
+}
